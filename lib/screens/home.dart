bbc93f761d5348c6777f2b7c878149620010ac9d
MANIDEEP <manideeptonangi@gmail.com>
2020-01-25 15:53:12 +0530

    home screen

---
+++ lib/screens/home.dart
@@ -0,0 +1,23 @@
+import 'package:Maintanence/widgets/Buttons.dart';
+import 'package:flutter/material.dart';
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inventory Management'),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Buttons(name: 'New',onpressed: (){},),
+              Buttons(name: 'Maintainence',onpressed: (){},)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
