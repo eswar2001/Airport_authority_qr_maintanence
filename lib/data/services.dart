0bc2effe54cfba44c871295f541f12703d45997f
sairam2000 <sairamnimmakayala8@gmail.com>
2020-01-25 23:56:27 +0530

    ui upgrade

---
+++ lib/data/services.dart
@@ -0,0 +1,17 @@
+import 'package:Maintanence/data%20structure/service.dart';
+//import 'package:flutter/material.dart';
+
+class Services{
+  List<ServiceData> servicelist =[
+    ServiceData(id: 001111,remarks: 'Remarksjdfttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt',date: DateTime.now(),cost: 250),
+    ServiceData(id: 0022342,remarks: 'Remarks',date: DateTime.now()),
+    ServiceData(id: 003245,remarks: 'Remarks',date: DateTime.now()),
+    ServiceData(id: 004234,remarks: 'Remarks',date: DateTime.now()),
+    ServiceData(id: 004234,remarks: 'Remarks',date: DateTime.now()),
+  ];
+
+  List<ServiceData> getServices(){
+    return servicelist;
+  }
+
+}
--- lib/screens/ServiceList.dart
+++ lib/screens/ServiceList.dart
@@ -1,5 +1,5 @@
 import 'package:Maintanence/data%20structure/service.dart';
-import 'package:Maintanence/data%20structure/services.dart';
+import 'package:Maintanence/data/services.dart';
 import 'package:flutter/material.dart';
 
 
@@ -27,12 +27,43 @@ Widget _myListView(BuildContext context) {
   return ListView.builder(
     itemCount: services.length,
     itemBuilder: (context, index) {
-      return Card(
-        child: ListTile(
-          leading: Text(services[index].id.toString()),
-          title: Text(services[index].remarks),
-          subtitle: Text(services[index].date.toIso8601String()),
-          trailing: Text(services[index].cost.toString()),
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0,horizontal: 16.0),
+        child: Card(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('product id :${services[index].id}',
+                style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: <Widget>[
+                    Text('complaint : ',
+                      style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),),
+                    Expanded(
+                      child: Text('${services[index].remarks}',
+                          style: TextStyle(fontSize: 16.0)),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Date :${services[index].date}',
+                  style: TextStyle(fontSize: 20.0),),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Cost :${services[index].cost}',
+                  style: TextStyle(fontSize: 20.0),),
+              )
+
+            ],
+          ),
         ),
       );
     },
